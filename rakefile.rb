e1bd818692976f93e53dfa8f7e92609bc0848e90
Jeff McAffee <jeff@ktechsystems.com>
2012-04-07 00:46:28 +0000

    Adding support for extraneous tasks. Removing gem tasks.

--- rakefile.rb
+++ rakefile.rb
@@ -8,7 +8,6 @@
 ######################################################################################
 
 require 'rubygems'
-require 'rubygems/package_task'
 require 'psych'
 gem 'rdoc', '>= 3.9.4'
 
@@ -18,11 +17,12 @@ require 'rdoc/task'
 require 'ostruct'
 require 'rakeUtils'
 
-# Setup common directory structure
 
 
+# Set the project name
 PROJNAME        = "PpmToGdl"
 
+# Bring in the library's version constant
 $:.unshift File.expand_path("../lib", __FILE__)
 require "ppmtogdl/version"
 
@@ -36,6 +36,7 @@ CLOBBER.include("pkg")
 #CLEAN.include("#{BUILDDIR}/**/*.*")
 #CLOBBER.include("#{BUILDDIR}")
 
+# Setup common directory structure ----------------------------------
 
 #directory BUILDDIR
 
@@ -63,48 +64,8 @@ end
 
 
 #############################################################################
-desc "Documentation for building gem"
-task :help do
-	hr = "-"*79
-	puts hr
-	puts "Building the Gem"
-	puts "================"
-	puts
-	puts "Use the following command line to build and install the gem"
-	puts 
-	puts "rake clean gem && gem install pkg\\#{PROJNAME.downcase}-#{PKG_VERSION}.gem -l --no-ri --no-rdoc"
-	puts
-	puts "See also the build_gem_script task which will create a cmd script to build and install the gem."
-	puts
-	puts hr
-end
-
-
-#############################################################################
-desc "Generate a simple script to build and install this gem"
-task :build_gem_script do
-	scriptname = "buildgem.cmd"
-	if(File.exists?(scriptname))
-		puts "Removing existing script."
-		rm scriptname
-	end
-	
-	File.open(scriptname, 'w') do |f|
-		f << "::\n"
-		f << ":: #{scriptname}\n"
-		f << "::\n"
-		f << ":: Running this script will generate and install the #{PROJNAME} gem.\n"
-		f << ":: Run 'rake build_gem_script' to regenerate this script.\n"
-		f << "::\n"
-
-		f << "rake clean gem && gem install pkg\\#{PROJNAME.downcase}-#{PKG_VERSION}.gem -l --no-ri --no-rdoc\n"
-	end
-end
-
-
-#############################################################################
 RDoc::Task.new(:rdoc) do |rdoc|
-    files = ['README.rdoc', 'docs/**/*.rdoc', 'lib/**/*.rb', 'app/**/*.rb']
+    files = ['README.rdoc', 'docs/**/*.rdoc', 'lib/**/*.rb', 'bin/**/*']
     rdoc.rdoc_files.add( files )
     rdoc.main = "README.rdoc"           	# Page to start on
 	#puts "PWD: #{FileUtils.pwd}"
@@ -115,17 +76,7 @@ end
 
 
 #############################################################################
-desc "List files to be included in gem"
-task :pkg_list do
-	puts "PKG_FILES (will be included in gem):"
-	PKG_FILES.each do |f|
-		puts "  #{f}"
-	end
-end
-
-
-#############################################################################
-spec = Gem::Specification.new do |s|
+SPEC = Gem::Specification.new do |s|
 	s.platform = Gem::Platform::RUBY
 	s.summary = "GDL PPM Generator Utility"
 	s.name = PROJNAME.downcase
@@ -142,14 +93,5 @@ spec = Gem::Specification.new do |s|
 	s.description = <<EOF
 PpmToGdl generates GDL PPM definitions from a PrimaryParameters.xml file.
 EOF
-end
-
-
-#############################################################################
-Gem::PackageTask.new(spec) do |pkg|
-	pkg.need_zip = true
-	pkg.need_tar = true
-	
-	puts "PKG_VERSION: #{PKG_VERSION}"
 end
 
