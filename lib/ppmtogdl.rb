939e10e76098cbc8ab68c3da519555b1091e1975
Jeff McAffee <jeff@ktechsystems.com>
2013-04-30 18:51:25 -0700

    Update rakefile and clean up require file.

--- lib/ppmtogdl.rb
+++ lib/ppmtogdl.rb
@@ -1,7 +1,7 @@
 ##############################################################################
 # File:: ppmtogdl.rb
 # Purpose:: Include file for PpmToGdl library
-# 
+#
 # Author::    Jeff McAffee 03/07/2010
 # Copyright:: Copyright (c) 2010, kTech Systems LLC. All rights reserved.
 # Website::   http://ktechsystems.com
@@ -11,24 +11,20 @@ require 'find'
 require 'logger'
 require 'win32ole'
 
-$LOGGING = true   # TODO: Change this flag to false when releasing production build.
 
 if(!$LOG)
-    if($LOGGING)
-      # Create a new log file each time:
-      file = File.open('ppmtogdl.log', File::WRONLY | File::APPEND | File::CREAT | File::TRUNC)
-      $LOG = Logger.new(file)
-      #$LOG = Logger.new('xledit.log', 2)
-      $LOG.level = Logger::DEBUG
-      #$LOG.level = Logger::INFO
-    else
-      $LOG = Logger.new(STDERR)
-      $LOG.level = Logger::ERROR
-    end
-    $LOG.info "**********************************************************************"
-    $LOG.info "Logging started for PpmToGdl library."
-    $LOG.info "**********************************************************************"
+  $LOG = Logger.new(STDERR)
+  $LOG.level = Logger::ERROR
 end
+
+if ENV["DEBUG"] == '1'
+  puts "LOGGING: ON due to DEBUG=1"
+  $LOG.level = Logger::DEBUG
+end
+
+$LOG.info "**********************************************************************"
+$LOG.info "Logging started for PpmToGdl library."
+$LOG.info "**********************************************************************"
 
 
 class_files = File.join( File.dirname(__FILE__), 'ppmtogdl', '*.rb')
--- rakefile.rb
+++ rakefile.rb
@@ -8,6 +8,7 @@
 ######################################################################################
 
 require 'rubygems'
+require 'rubygems/package_task'
 require 'psych'
 gem 'rdoc', '>= 3.9.4'
 
@@ -15,19 +16,20 @@ require 'rake'
 require 'rake/clean'
 require 'rdoc/task'
 require 'ostruct'
-require 'rakeUtils'
 
 
 
 # Set the project name
 PROJNAME        = "PpmToGdl"
 
+PKGDIR          = "pkg"
+
 # Bring in the library's version constant
 $:.unshift File.expand_path("../lib", __FILE__)
 require "ppmtogdl/version"
 
-PKG_VERSION	= PpmToGdl::VERSION
-PKG_FILES 	= Dir["**/*"].select { |d| d =~ %r{^(README|bin/|data/|ext/|lib/|spec/|test/)} }
+PKG_VERSION = PpmToGdl::VERSION
+PKG_FILES   = Dir["**/*"].select { |d| d =~ %r{^(README|bin/|data/|ext/|lib/|spec/|test/)} }
 
 # Setup common clean and clobber targets
 
@@ -41,19 +43,6 @@ CLOBBER.include("pkg")
 #directory BUILDDIR
 
 $verbose = true
-	
-
-#############################################################################
-#### Imports
-# Note: Rake loads imports only after the current rakefile has been completely loaded.
-
-# Load local tasks.
-imports = FileList['tasks/**/*.rake']
-imports.each do |imp|
-	puts "Importing local task file: #{imp}" if $verbose
-	import "#{imp}"
-end
-
 
 
 #############################################################################
@@ -65,10 +54,10 @@ end
 
 #############################################################################
 RDoc::Task.new(:rdoc) do |rdoc|
-    files = ['README.rdoc', 'docs/**/*.rdoc', 'lib/**/*.rb', 'bin/**/*']
+    files = ['docs/**/*.rdoc', 'lib/**/*.rb', 'bin/**/*']
     rdoc.rdoc_files.add( files )
-    rdoc.main = "README.rdoc"           	# Page to start on
-	#puts "PWD: #{FileUtils.pwd}"
+    rdoc.main = "docs/README.rdoc"             # Page to start on
+  #puts "PWD: #{FileUtils.pwd}"
     rdoc.title = "#{PROJNAME} Documentation"
     rdoc.rdoc_dir = 'doc'                   # rdoc output folder
     rdoc.options << '--line-numbers' << '--all'
@@ -76,22 +65,63 @@ end
 
 
 #############################################################################
-SPEC = Gem::Specification.new do |s|
-	s.platform = Gem::Platform::RUBY
-	s.summary = "GDL PPM Generator Utility"
-	s.name = PROJNAME.downcase
-	s.version = PKG_VERSION
-	s.requirements << 'none'
-	s.bindir = 'bin'
-	s.require_path = 'lib'
-	#s.autorequire = 'rake'
-	s.files = PKG_FILES
-	s.executables = "ppmtogdl"
-	s.author = "Jeff McAffee"
-	s.email = "gems@ktechdesign.com"
-	s.homepage = "http://gems.ktechdesign.com"
-	s.description = <<EOF
+desc "List files to be included in gem"
+task :pkg_list do
+  puts "PKG_FILES (will be included in gem):"
+  PKG_FILES.each do |f|
+    puts "  #{f}"
+  end
+end
+
+
+#############################################################################
+spec = Gem::Specification.new do |s|
+  s.platform = Gem::Platform::RUBY
+  s.summary = "GDL PPM Generator Utility"
+  s.name = PROJNAME.downcase
+  s.version = PKG_VERSION
+  s.requirements << 'none'
+  s.bindir = 'bin'
+  s.require_path = 'lib'
+  #s.autorequire = 'rake'
+  s.files = PKG_FILES
+  s.executables = "ppmtogdl"
+  s.author = "Jeff McAffee"
+  s.email = "gems@ktechdesign.com"
+  s.homepage = "http://gems.ktechdesign.com"
+  s.description = <<EOF
 PpmToGdl generates GDL PPM definitions from a PrimaryParameters.xml file.
 EOF
 end
+
+
+#############################################################################
+Gem::PackageTask.new(spec) do |pkg|
+  pkg.need_zip = true
+  pkg.need_tar = true
+
+  puts "PKG_VERSION: #{PKG_VERSION}"
+end
+
+
+#############################################################################
+desc "Run all tests"
+task :test => [:init] do
+  unless File.directory?('test')
+    $stderr.puts 'no test in this package'
+    return
+  end
+  $stderr.puts 'Running tests...'
+  begin
+    require 'test/unit'
+  rescue LoadError
+    $stderr.puts 'test/unit cannot loaded.  You need Ruby 1.8 or later to invoke this task.'
+  end
+
+  $LOAD_PATH.unshift("./")
+  $LOAD_PATH.unshift(TESTDIR)
+  Dir[File.join(TESTDIR, "*.rb")].each {|file| require File.basename(file) }
+  require 'minitest/autorun'
+end
+
 
